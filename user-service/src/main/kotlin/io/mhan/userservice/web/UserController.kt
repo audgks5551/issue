11962bfe9637f8b220e1b69f1f74d4fe4f8caf3b
audgks5551 <audgks5551@daum.net>
2022-12-24 01:04:16 +0900

    내정보 수정 구현, 이미지 조회 구현

--- user-service/src/main/kotlin/io/mhan/userservice/web/UserController.kt
+++ user-service/src/main/kotlin/io/mhan/userservice/web/UserController.kt
@@ -1,12 +1,14 @@
 package io.mhan.userservice.web
 
 import io.mhan.userservice.domain.service.UserService
-import io.mhan.userservice.model.AuthToken
-import io.mhan.userservice.model.MeResponse
-import io.mhan.userservice.model.SignInRequest
-import io.mhan.userservice.model.SignUpRequest
+import io.mhan.userservice.model.*
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.springframework.core.io.ClassPathResource
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.*
+import java.io.File
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -34,5 +36,32 @@ class UserController(
         @AuthToken token: String
     ) : MeResponse {
         return MeResponse(userService.getByToken(token))
+    }
+
+    @GetMapping("/{userId}/username")
+    suspend fun getUsername(@PathVariable userId: Long) : Map<String, String> {
+        return mapOf("reporter" to userService.get(userId).username)
+    }
+
+    @PostMapping("/{id}", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    suspend fun edit(
+        @PathVariable id: Long,
+        @ModelAttribute request: UserEditRequest,
+        @AuthToken token: String,
+        @RequestPart("profileUrl") filePart: FilePart
+    ) {
+        val orgFilename = filePart.filename()
+        var filename: String? = null
+
+        if (orgFilename.isNotEmpty()) {
+            // abc.jpg
+            val ext = orgFilename.substring(orgFilename.lastIndexOf(".") + 1)
+            filename = "${id}.${ext}"
+
+            val file = File(ClassPathResource("/images/").file, filename)
+            filePart.transferTo(file).awaitSingleOrNull()
+        }
+
+        userService.edit(token, request.username, filename)
     }
 }
